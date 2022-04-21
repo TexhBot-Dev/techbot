270525721965521a2cd7f9277aded4e59cd06b5b
Scott Gould <greysilly7@gmail.com>
2022-04-21 13:37:02 -0400

    Feat(hunt): Added a hunting command

--- src/commands/economy/hunt.ts
+++ src/commands/economy/hunt.ts
@@ -1,6 +1,29 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { ApplicationCommandRegistry, Command, CommandOptions } from '@sapphire/framework';
 import type { CommandInteraction } from 'discord.js';
+import { addToWallet } from '../../lib/helpers/economy';
+
+const animals = [
+	'pig',
+	'cow',
+	'chicken',
+	'rabbit',
+	'horse',
+	'cat',
+	'dog',
+	'elephant',
+	'giraffe',
+	'lion',
+	'zebra',
+	'rhino',
+	'bear',
+	'panda',
+	'koala',
+	'penguin',
+	'parrot',
+	'polar bear',
+	'polar bear cub'
+];
 
 @ApplyOptions<CommandOptions>({
 	name: 'hunt',
@@ -9,13 +32,22 @@ import type { CommandInteraction } from 'discord.js';
 })
 export class HuntCommand extends Command {
 	async chatInputRun(interaction: CommandInteraction) {
-		// TODO: Implement
-		return interaction.reply('Not implemented yet');
+		await interaction.reply('Searching for an Animal...');
+
+		if (Math.random() > 0.5) return interaction.editReply("You didn't find any Animals.");
+
+		// Get random item from items
+		const animal = animals[Math.floor(Math.random() * animals.length)];
+		// generate random amount of money no greater than 100
+		const money = Math.floor(Math.random() * 100) + 1;
+
+		await addToWallet(interaction.user, money);
+		return interaction.editReply(`You culled a(n) ${animal} and earned ${money} coins!`);
 	}
 
 	registerApplicationCommands(registry: ApplicationCommandRegistry) {
 		registry.registerChatInputCommand((builder) => builder.setName(this.name).setDescription(this.description), {
-			idHints: ['944645717232848907']
+			idHints: ['966754372199542794']
 		});
 	}
 }
