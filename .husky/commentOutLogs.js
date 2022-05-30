c891a2490b7565853bb1fe8b3435a1f91086190c
Spen <spenmusic6@gmail.com>
2022-05-30 10:30:31 -0400

    testing out new system

--- .husky/commentOutLogs.js
+++ .husky/commentOutLogs.js
@@ -1,6 +1,8 @@
 import { promises } from 'fs';
 import { join, resolve } from 'path';
 
+console.info("Removing console.logs...");
+
 async function* walk(dir) {
 	for await (const d of await promises.opendir(dir)) {
 		const entry = join(dir, d.name);
@@ -9,7 +11,14 @@ async function* walk(dir) {
 	}
 }
 
+let replacedCount = 0;
 for await (const p of walk(resolve('./src/commands'))) {
 	const contents = await promises.readFile(p, { encoding: 'utf8' });
-	promises.writeFile(p, contents.replace(/console\.[a-zA-Z]+\([^\)]+\);?/g, ''));
+	promises.writeFile(p, contents.replace(/(console\.[a-zA-Z]+\([^\)]+\);?)/g, (match) => {
+		replacedCount++
+		return "// " + match;
+	}));
 }
+
+
+console.info(`Commented out ${replacedCount} console.logs. Consider removing them completely.`)
